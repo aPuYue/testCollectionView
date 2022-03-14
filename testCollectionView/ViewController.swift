5aba0f9210bb385bf416f9f2eac157ddbc8dcb90
puyue <pu.yue@access-company.com>
2022-03-14 13:57:33 +0900

    1

---
+++ testCollectionView/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  testCollectionView
+//
+//  Created by user on 2021/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+extension ViewController : UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    
+}
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
+
